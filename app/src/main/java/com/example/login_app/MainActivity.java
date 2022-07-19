b0546b3859bd39560d535a02a499c126d1f1f629
Joynamahen <mjoyna4398@gmail.com>
2022-07-19 22:07:01 +0530

    Add images

--- app/src/main/java/com/example/login_app/MainActivity.java
+++ app/src/main/java/com/example/login_app/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.login_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (Button) findViewById()
+
     }
 }
